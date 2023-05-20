39abf0334fb9565b73544124cc8ba5bc3ca798ba
Alba Arenas <108294869+albscr@users.noreply.github.com>
2023-05-20 15:29:43 -0600

    Added project n.4
    Added project n.4 named "TrendyZone" in de position n.2 of mi Projects page

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -4,8 +4,8 @@ const projectsData = [
         id: 1,
         title: "Canal Street Market",
         photo: "https://user-images.githubusercontent.com/108294869/238160852-e36ac5ab-6476-4d93-9882-797e546e83af.png",
-        description: 
-        "I developed a project to create an e-commerce platform using modern web development technologies. Although my approach was initially based on an existing online store, I eventually decided to adapt the project to create a standalone e-commerce platform. During the development process, I used multiple tools and technologies, such as ReactJS, React Router 6, Framer Motion, Sass and learned a lot about how to work with these technologies together. In addition, the project allowed me to improve my programming skills and taught me how to create a web application from scratch.",
+        description:
+            "I developed a project to create an e-commerce platform using modern web development technologies. Although my approach was initially based on an existing online store, I eventually decided to adapt the project to create a standalone e-commerce platform. During the development process, I used multiple tools and technologies, such as ReactJS, React Router 6, Framer Motion, Sass and learned a lot about how to work with these technologies together. In addition, the project allowed me to improve my programming skills and taught me how to create a web application from scratch.",
         links: [
             { live: "https://canal-street-kappa.vercel.app/" },
             { repository: "https://github.com/albscr/Canal-Street" }
@@ -15,18 +15,32 @@ const projectsData = [
     },
     {
         id: 2,
+        title: "TrendyZone",
+        photo: "https://user-images.githubusercontent.com/108294869/239708987-c1c86c81-5093-4e68-bbe1-a4bbecb1407e.png",
+        description: "The TrendyZone project focused on integrating an API to simulate a realistic shopping experience. State was efficiently managed and contexts were created for user authentication and shopping cart. Private and public routes were implemented to ensure data security. In addition, full shopping cart functionality was achieved, allowing products to be added, searched and grouped by category. LocalStorage was used to store purchase orders and a responsive design was applied.",
+        links: [
+            { live: "https://trendy-zone.vercel.app/" },
+            { repository: "https://github.com/albscr/TrendyZone" }
+        ],
+        library: "React + Vite.js",
+        plus: "Tailwind"
+
+    },
+    {
+        id: 3,
         title: "Parallax Effect",
         photo: "https://user-images.githubusercontent.com/108294869/238160891-ee166b00-4d79-4d7b-bb5e-f5a0054c7817.png",
-        description:  "I have experience using various CSS properties and techniques to enhance the appearance and functionality of web pages. This includes manipulating elements on the z-axis with translate3d(), adjusting values using calc(), using background-position to manipulate section backgrounds, and improving animation performance with will-change. I also use properties like scroll-behavior and overflow to create a smooth and seamless browsing experience. Additionally, I use text-shadow to add visual interest to text and window.addEventListener() to detect user scroll events and update CSS variables accordingly.",
+        description: "I have experience using various CSS properties and techniques to enhance the appearance and functionality of web pages. This includes manipulating elements on the z-axis with translate3d(), adjusting values using calc(), using background-position to manipulate section backgrounds, and improving animation performance with will-change. I also use properties like scroll-behavior and overflow to create a smooth and seamless browsing experience. Additionally, I use text-shadow to add visual interest to text and window.addEventListener() to detect user scroll events and update CSS variables accordingly.",
         links: [
             { live: "https://prueba-parallax.vercel.app/" },
             { repository: "https://github.com/albscr/Parallax-effect" }
         ],
         library: "JavaScript",
-        
+
     },
+
     {
-        id: 3,
+        id: 4,
         title: "VideoGame",
         photo: "https://user-images.githubusercontent.com/108294869/238160924-8079da7f-758c-4789-a08c-f92305020bf1.png",
         description: "I implemented events in the browser window using window.addEventListener to improve the interactivity of a game. I also used my knowledge of mathematics to create game logic, including collision detection, path calculation, and object animation. To create custom graphics and game elements, I used JavaScript canvas, adjusting sizes and positions to fit the user interface. In addition, I created and manipulated multidimensional arrays to store and organize relevant game data, such as object positions and player statistics. I also implemented custom events and buttons to improve the usability of the game and allow players to interact with it more intuitively. Finally, I used localStorage to store and retrieve relevant game data, such as player score and level progress.",
@@ -36,6 +50,6 @@ const projectsData = [
         ],
         library: "JavaScript",
     },
-]; 
+];
 
 export default projectsData;
